a469b68927a85af44995c62d0aea377eb5cffae6
TheBunnyMan123 <bunny@tkbunny.net>
2025-03-15 14:01:14 -0500

    Add screens and peripherals.

---
+++ modules/peripherals.lua
@@ -0,0 +1,185 @@
+if peripheral.getNames and debug then
+   local i, key, value = 1, debug.getupvalue(peripheral.isPresent, 2)
+   while key ~= "native" and key ~= nil do
+      key, value = debug.getupvalue(peripheral.isPresent, i)
+      i=i+1
+   end
+   _G.peripheral = value or peripheral
+end
+local native = peripheral
+peripheral = nil ---@diagnostic disable-line lowercase-global
+
+---@class HydraKernel.Peripherals
+local lib = {}
+
+local sides = {
+   "bottom", "top", "back", "front", "left"
+}
+
+---Returns a list of peripherals
+---@return string[]
+function lib.getPeripherals()
+   local names = {}
+
+   for _, side in ipairs(sides) do
+      if native.isPresent(side) then
+         names[#names + 1] = side
+         if native.hasType(side, "peripheral_hub") then
+            local remote = native.call(side, "getNamesRemote")
+
+            for _, name in ipairs(remote) do
+               names[#names + 1] = name
+            end
+         end
+      end
+   end
+
+   return names
+end
+
+---Returns whether a peripheral is present
+---@param name string
+---@return boolean
+function lib.isPresent(name)
+   if native.isPresent(name) then
+      return true
+   end
+
+   for _, side in ipairs(sides) do
+      if native.isPresent(side) then
+         if native.hasType(side, "peripheral_hub") and native.call(side, "isPresentRemote", name) then
+            return true
+         end
+      end
+   end
+
+   return false
+end
+
+---Returns the type of a peripheral
+---@param name string
+---@return string?
+function lib.type(name)
+   if native.isPresent(name) then
+      return native.getType(name)
+   end
+
+   for _, side in ipairs(sides) do
+      if native.isPresent(side) then
+         if native.hasType(side, "peripheral_hub") and native.call(side, "isPresentRemote", name) then
+            return native.call(side, "getTypeRemote", name)
+         end
+      end
+   end
+end
+
+---Checks whether a peripheral has a specific type
+---@param name string
+---@param type string
+---@return boolean
+function lib.hasType(name, type)
+   if native.isPresent(name) then
+      return native.hasType(name, type)
+   end
+
+   for _, side in ipairs(sides) do
+      if native.isPresent(side) then
+         if native.hasType(side, "peripheral_hub") and native.call(side, "isPresentRemote", name) then
+            return native.call(side, "hasTypeRemote", name, type)
+         end
+      end
+   end
+
+   return false
+end
+
+---Returns the methods a peripheral has
+---@param name string
+---@return string[]?
+function lib.getMethods(name)
+   if native.isPresent(name) then
+      return native.getMethods(name)
+   end
+
+   for _, side in ipairs(sides) do
+      if native.isPresent(side) then
+         if native.hasType(side, "peripheral_hub") and native.call(side, "isPresentRemote", name) then
+            return native.call(side, "getMethodsRemote", name)
+         end
+      end
+   end
+end
+
+---Calls a method on a peripheral
+---@param name string
+---@param method string
+---@param ... unknown
+---@return unknown?
+function lib.call(name, method, ...)
+   if native.isPresent(name) then
+      return native.call(name, method, ...)
+   end
+
+   for _, side in ipairs(sides) do
+      if native.isPresent(side) then
+         if native.hasType(side, "peripheral_hub") and native.call(side, "isPresentRemote", name) then
+            return native.call(side, "callRemote", name, method, ...)
+         end
+      end
+   end
+end
+
+---Returns the type of a peripheral
+---@param name string
+---@return string?
+function lib.getType(name)
+   if native.isPresent(name) then
+      return native.getType(name)
+   end
+
+   for _, side in ipairs(sides) do
+      if native.isPresent(side) then
+         if native.hasType(side, "peripheral_hub") and native.call(side, "isPresentRemote", name) then
+            return native.call(side, "getTypeRemote", name)
+         end
+      end
+   end
+end
+
+---Wraps a peripheral
+---@param name string
+---@return {name: string, type: string, [string]: function}
+function lib.wrap(name)
+   local wrapped = {name = name, type = lib.getType(name)}
+
+   for _, method in lib.getMethods(name) do
+      wrapped[method] = function(...)
+         lib.call(name, method, ...)
+      end
+   end
+
+   return wrapped
+end
+
+---Gets and wraps every peripheral of a specific type based on an optional filter
+---@param type any
+---@param filter fun(name: string, peripheral: {name: string, type: string, [string]: function}): boolean
+---@return {name: string, type: string, [string]: function}[]
+function lib.find(type, filter)
+   filter = filter or function() return true end
+
+   local peripherals = {}
+   for _, name in ipairs(lib.getPeripherals()) do
+      if lib.hasType(name, type) then
+         local wrapped = peripheral.wrap(name)
+
+         if filter(name, wrapped) then
+            table.insert(peripherals, wrapped)
+         end
+      end
+   end
+   return peripherals
+end
+
+return lib
+
---
+++ modules/screen.lua
@@ -0,0 +1,223 @@
+local lib = {}
+---@class HydraKernel.Screen
+---@field native table
+local screen = {}
+local screenMt = {
+   __index = screen,
+   __type = "HydraKernel.Screen"
+}
+
+---Writes text to the screen.
+---@param self HydraKernel.Screen
+---@param text string
+---@param wrap boolean?
+function screen.write(self, text, wrap)
+   if wrap then
+      local x, y = self:getCursorPos()
+      local width = self:getWidth()
+
+      self.native.write(text:sub(1, width - x))
+
+      local iter = 0
+      for i = (width - x), #text, width do
+         iter = iter + 1
+
+         if (y + iter) > width then
+            self:scroll(1)
+            y = y - 1
+         end
+         self:setCursorPos(1, y + iter)
+
+         self.native:write(text:sub(i, i + width - 1))
+      end
+   else
+      self.native.write(text)
+   end
+end
+
+---Scrolls by `y` lines
+---@param self HydraKernel.Screen
+---@param amount integer
+function screen.scroll(self, amount)
+   self.native.scroll(amount)
+end
+
+---Returns the current cursor position
+---@param self HydraKernel.Screen
+---@return integer
+---@return integer
+function screen.getCursorPos(self)
+   return self.native.getCursorPos()
+end
+
+---Sets the cursor's position
+---@param self HydraKernel.Screen
+---@param x integer
+---@param y integer
+function screen.setCursorPos(self, x, y)
+   self.native.setCursorPos(x, y)
+end
+
+---Returns whether or not the cursor is blinking
+---@param self HydraKernel.Screen
+---@return boolean
+function screen.isCursorBlinking(self)
+   return self.native.getCursorBlink()
+end
+
+---Sets whether the cursor is blinking
+---@param self HydraKernel.Screen
+---@param state boolean
+function screen.setCursorBlinking(self, state)
+   self.native.setCursorBlink(state)
+end
+
+---Returns the screen's size
+---@param self HydraKernel.Screen
+---@return integer
+---@return integer
+function screen.getSize(self)
+   return self.native.getSize()
+end
+
+---Gets the screen's width
+---@param self HydraKernel.Screen
+---@return integer
+function screen.getWidth(self)
+   local ret = self:getSize()
+   return ret
+end
+
+---Gets the screen's height
+---@param self HydraKernel.Screen
+---@return integer
+function screen.getHeight(self)
+   local _, ret = self:getSize()
+   return ret
+end
+
+---Clears the screen
+---@param self HydraKernel.Screen
+function screen.clear(self)
+   self.native.clear()
+end
+
+---Clears the line the cursor is on
+---@param self HydraKernel.Screen
+function screen.clearLine(self)
+   self.native.clearLine()
+
+   local _, y = self:getCursorPos()
+   self:setCursorPos(1, y)
+end
+
+---Returns the current text color
+---@param self HydraKernel.Screen
+---@return integer
+function screen.getTextColor(self)
+   return self.native.getTextColor()
+end
+
+---Sets the current text color
+---@param self HydraKernel.Screen
+---@param color integer
+function screen.setTextColor(self, color)
+   self.native.setTextColor(color)
+end
+
+---Returns the current background color
+---@param self HydraKernel.Screen
+---@return integer
+function screen.getBackgroundColor(self)
+   return self.native.getBackgroundColor()
+end
+
+---Sets the current background color
+---@param self HydraKernel.Screen
+---@param color integer
+function screen.setBackgroundColor(self, color)
+   self.native.setBackgroundColor(color)
+end
+
+---Returns whether or not the screen supports all 16 colors
+---@param self HydraKernel.Screen
+---@return boolean
+function screen.isColor(self)
+   return self.native.isColor()
+end
+
+---Writes text with specific colors
+---@param self HydraKernel.Screen
+---@param text string
+---@param textColor string
+---@param backgroundColor string
+---@param wrap boolean
+function screen.blit(self, text, textColor, backgroundColor, wrap)
+   if wrap then
+      local x, y = self:getCursorPos()
+      local width = self:getWidth()
+
+      self.native.blit(text:sub(1, width - x), textColor:sub(1, width - x), backgroundColor:sub(1, width - x))
+
+      local iter = 0
+      for i = (width - x), #text, width do
+         iter = iter + 1
+
+         if (y + iter) > width then
+            self:scroll(1)
+            y = y - 1
+         end
+         self:setCursorPos(1, y + iter)
+
+         self.native.blit(text:sub(i, i + width - 1), textColor:sub(i, i + width - 1), backgroundColor:sub(i, i + width - 1))
+      end
+   else
+      self.native.blit(text, textColor, backgroundColor)
+   end
+end
+
+---Gets the true color for a color in the palette
+---@param self HydraKernel.Screen
+---@param index integer
+---@return integer
+function screen.getPaletteColor(self, index)
+   return self.native.getPaletteColor(index)
+end
+
+---Sets the true color for a color in the palette
+---@param self HydraKernel.Screen
+---@param index integer
+---@param color integer
+function screen.setPaletteColor(self, index, color)
+   self.native.setPaletteColor(index, color)
+end
+
+
+local function wrap(monitor)
+   return setmetatable({native = (monitor.native and monitor.native() or monitor)}, screenMt)
+end
+
+local screens = {
+   [0] = wrap(term)
+}
+---@diagnostic disable-next-line lowercase-global
+term = nil
+
+function lib.get(id)
+   return screens[id]
+end
+
+local peripherals = require((...):gsub("%.screen$", "") .. ".peripherals")
+coroutine.resume(coroutine.create(function()
+   local native = screens[0]
+   while true do
+      screens = {[0] = native}
+
+      for _, monitor in peripherals.find("monitor") do
+         screens[#screens + 1] = wrap(monitor)
+      end
+   end
+end))
+
+return lib
+
